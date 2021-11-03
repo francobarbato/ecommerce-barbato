0bd7ff58691fc450047a1546a46fe91bcdfd6e31
Franco Barbato <franco.barbato09@gmail.com>
2021-11-03 02:36:10 -0300

    oredenando carpetas

--- ecommerce-barbato/src/components/navbar.js
+++ ecommerce-barbato/src/components/navbar.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-        <a class="navbar-brand" href="#">ShiShop</a>
+        <a class="navbar-brand" href="#">SkiShop</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
