e8060865ab803d8053ecb8374dbfb9a2c4d2cae5
Franco Barbato <franco.barbato09@gmail.com>
2021-11-05 14:59:40 -0300

    agregando imagen

--- ecommerce-barbato/src/componentes/Cart.js
+++ ecommerce-barbato/src/componentes/Cart.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import foto from './carrito.png';
+import foto from './imagenes/carrito.png';
 
 function Cart() {
     return (
       <>
         <button className="button" className="carrito" type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                  {/* <img src={foto} alt=""/> */}
+                  <img src={foto} alt=""/>
                 <span id="contador" className="badge bg-dark text-white ms-1 rounded-pill" className="sr-only">0</span>
         </button>
       </>
