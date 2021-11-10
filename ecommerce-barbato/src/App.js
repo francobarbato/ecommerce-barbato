462c5261eeb36450de8945e72bb2e14ec4e6f425
Franco Barbato <franco.barbato09@gmail.com>
2021-11-10 18:13:41 -0300

    ItemCount

--- ecommerce-barbato/src/App.js
+++ ecommerce-barbato/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './componentes/NavBar';
-// import Cart from './componentes/Cart';
 import ItemListContainer from './componentes/ItemListContainer';
 
 function App() {
@@ -10,13 +9,13 @@ function App() {
     <NavBar/>
     <h2>Cascos</h2>
       <div className="tarjeta">
-        <ItemListContainer marca="Shred" color="Negro"/>
+        <ItemListContainer marca="Shred" color="Color: Negro" cantidad="Unidades disponibles:" stock="5"/>
       </div>
       <div className="tarjeta">
-        <ItemListContainer marca="Salomon" color="Rojo"/>
+        <ItemListContainer marca="Salomon" color="Rojo" cantidad="Unidades disponibles:" stock="10"/>
       </div>
       <div className="tarjeta">
-        <ItemListContainer marca="Blizzard" color="Verde"/>
+        <ItemListContainer marca="Blizzard" color="Verde" cantidad="Unidades disponibles:" stock="8"/>
       </div>
     </>
   );
--- ecommerce-barbato/src/componentes/ItemListContainer.js
+++ ecommerce-barbato/src/componentes/ItemListContainer.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import ItemCount from './ItemCount'; 
 
 function ItemListContainer(props) {
     return (
       <>
         <h3>{props.marca}</h3>
         <div>{props.color}</div>
+        <div>{props.cantidad}</div>
+        <div>{props.stock}</div>
+        <hr/>
+        <ItemCount stock={props.stock} initial={1}/>
       </>
     );
 }
